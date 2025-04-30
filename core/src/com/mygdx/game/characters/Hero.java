0774db5f8fd071653b97a791e2099812aced7001
JulOseledko <joseledko@bk.ru>
2025-04-30 19:28:41 +0300

    Update Hero animation, HUD strings, and medkit feedback

--- core/src/com/mygdx/game/characters/Hero.java
+++ core/src/com/mygdx/game/characters/Hero.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameScreen;
@@ -12,6 +13,7 @@ import com.mygdx.game.Item;
 import com.mygdx.game.Weapon;
 
 public class Hero extends GameCharacter {
+    private String name;
     private int coins;
     private int level;
     private int exp;
@@ -19,28 +21,41 @@ public class Hero extends GameCharacter {
 
     public Hero(GameScreen gameScreen) {
         this.gameScreen = gameScreen;
+        this.name = "Alexander";
         this.level = 1;
-        this.texture = new Texture("Knight.png");
+        this.texture = new Texture("Knight_anim.png");
+        this.regions = new TextureRegion(texture).split(80, 80)[0];
         this.textureHp = new Texture("Bar.png");
         this.position = new Vector2(MathUtils.random(0, 1280), MathUtils.random(0, 720));
         while (!gameScreen.getMap().isCellPassable(position)) {
             this.position.set(MathUtils.random(0, 1280), MathUtils.random(0, 720));
         }
-        this.temp = new Vector2(0, 0);
         this.direction = new Vector2(0, 0);
         this.hpMax = 100.0f;
         this.hp = this.hpMax;
         this.speed = 100.0f;
-        this.weapon = new Weapon("Spear", 150.0f, 1.0f, 5.0f);
+        this.weapon = new Weapon("Spear", 80.0f, 1.0f, 4.0f);
+        this.secondsPerFrame =0.3f;
     }
 
     public void renderHUD(SpriteBatch batch, BitmapFont font24) {
-        font24.draw(batch, "Knight: Jul\nLevel: " + level + "\nExp: " + exp + " / " + expTo[level + 1] + "\nCoins: " + coins, 20, 700);
+        stringHelper.setLength(0);
+        stringHelper.append("Knight: ").append(name).append('\n')
+                .append("Level: ").append(level).append('\n')
+                .append("Exp: ").append(exp).append(" / ").append(expTo[level + 1]).append('\n')
+                .append("Coins: ").append(coins);
+        font24.draw(batch, stringHelper, 20, 700);
+    }
+    @Override
+    public void render(SpriteBatch batch, BitmapFont font24) {
+        super.render(batch, font24);
     }
 
     @Override
     public void update(float dt) {
         damageEffectTimer -= dt;
+        animationTimer += dt;
+
         if (damageEffectTimer < 0.0f) {
             damageEffectTimer = 0.0f;
         }
@@ -77,11 +92,7 @@ public class Hero extends GameCharacter {
             direction.y = -1.0f;
         }
 
-        temp.set(position).mulAdd(direction, speed * dt);
-        if (gameScreen.getMap().isCellPassable(temp)) {
-            position.set(temp);
-        }
-
+        moveForward(dt);
         checkScreenBounds();
     }
 
@@ -98,7 +109,20 @@ public class Hero extends GameCharacter {
     public void useItem(Item it) {
         switch (it.getType()) {
             case COINS:
-                coins += MathUtils.random(3, 5);
+                int amount = MathUtils.random(3, 5);
+                coins += amount;
+                stringHelper.setLength(0);
+                stringHelper.append("coins ").append("+").append(amount);
+                gameScreen.getTextEmitter().setup(it.getPosition().x, it.getPosition().y, stringHelper);
+                break;
+            case MEDKIT:
+                hp += 5;
+                stringHelper.setLength(0);
+                stringHelper.append("hp ").append("+5");
+                gameScreen.getTextEmitter().setup(it.getPosition().x, it.getPosition().y, stringHelper);
+                if (hp > hpMax) {
+                    hp = hpMax;
+                }
                 break;
         }
         it.deactivate();
