39274505a35818a73ecdf5919f180eb99a323dcc
JulOseledko <joseledko@bk.ru>
2025-05-03 16:49:25 +0300

    Update Monster animation and movement timing

--- core/src/com/mygdx/game/characters/Monster.java
+++ core/src/com/mygdx/game/characters/Monster.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 import com.mygdx.game.GameScreen;
@@ -15,24 +16,27 @@ public class Monster extends GameCharacter {
 
     public Monster(GameScreen gameScreen) {
         this.texture = new Texture("Skeleton.png");
+        this.regions = new TextureRegion(texture).split(80, 80)[0];
         this.textureHp = new Texture("Bar.png");
         this.position = new Vector2(MathUtils.random(0, 1280), MathUtils.random(0, 720));
         while (!gameScreen.getMap().isCellPassable(position)) {
             this.position.set(MathUtils.random(0, 1280), MathUtils.random(0, 720));
         }
         this.direction = new Vector2(0, 0);
-        this.temp = new Vector2(0, 0);
         this.speed = 40.0f;
         this.activityRadius = 200.0f;
         this.gameScreen = gameScreen;
         this.hpMax = 20;
         this.hp = this.hpMax;
         this.weapon = new Weapon("Rusty Sword", 50.0f, 0.8f, 5.0f);
+        this.secondsPerFrame = 0.3f;
     }
 
     @Override
     public void update(float dt) {
-        damageEffectTimer -= dt;
+        damageEffectTimer -= dt / 3.0f;
+        animationTimer += dt;
+
         if (damageEffectTimer < 0.0f) {
             damageEffectTimer = 0.0f;
         }
@@ -48,10 +52,8 @@ public class Monster extends GameCharacter {
                 direction.nor();
             }
         }
-        temp.set(position).mulAdd(direction, speed * dt);
-        if (gameScreen.getMap().isCellPassable(temp)) {
-            position.set(temp);
-        }
+
+        moveForward(dt);
 
         if (dst < weapon.getAttackRadius()) {
             attackTimer += dt;
