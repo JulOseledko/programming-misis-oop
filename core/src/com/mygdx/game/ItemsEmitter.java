8ff4098dce2de53dd88e4bc77708a982f278ac80
JulOseledko <joseledko@bk.ru>
2025-06-06 19:07:18 +0300

    Finalize desktop launcher, item emitter, and updated assets

--- core/src/com/mygdx/game/ItemsEmitter.java
+++ core/src/com/mygdx/game/ItemsEmitter.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 
-public class ItemsEmitter {
+public class ItemsEmitter extends FlyingText {
     private Item[] items;
     private Texture texture;
     private TextureRegion[] regions;
--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -9,6 +9,8 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = 1280;
 		config.height = 720;
+		config.fullscreen = false;
+		config.resizable = true;
 		new LwjglApplication(new MyGdxGame(), config);
 	}
 }
